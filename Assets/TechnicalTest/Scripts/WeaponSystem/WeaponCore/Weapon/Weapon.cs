56dc4b9b21e0f847bd45e67d1aa9ec1e53c60bdf
Abraxas170600 <abraxassabaoth666@gmail.com>
2024-03-31 23:09:10 -0500

    Weapons configured for implementation of bullet features

--- Assets/TechnicalTest/Scripts/WeaponSystem/WeaponCore/Weapon/Weapon.cs
+++ Assets/TechnicalTest/Scripts/WeaponSystem/WeaponCore/Weapon/Weapon.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TechnicalTest.System.WeaponSystem.Data;
 using UnityEngine;
 
@@ -6,20 +7,42 @@ namespace TechnicalTest.System.WeaponSystem.WeaponCore
     public class Weapon : MonoBehaviour
     {
         [SerializeField] private WeaponType weaponType;
-        [SerializeField] private GameObject triggerDetector;
 
         private Rigidbody weaponRigidbody;
         private BoxCollider weaponCollider;
         private SphereCollider weaponTrigger;
 
-        private uint weaponDamage;
         private float weaponAttackSpeed;
+        private float attackSpeedTimer;
+        private bool canShoot;
 
-        public void Set(uint weaponDamage, float weaponAttackSpeed)
+        [SerializeField] private GameObject bulletSpawn;
+        [SerializeField] private int bulletSize = 10;
+        private readonly List<GameObject> bullets = new List<GameObject>();
+
+
+        #region Initialize
+        public void InitializeWeapon(uint weaponDamage, float weaponAttackSpeed, Bullet weaponBullet)
         {
-            this.weaponDamage = weaponDamage;
             this.weaponAttackSpeed = weaponAttackSpeed;
+            InstantiateWeaponBullets(weaponBullet, weaponDamage, bulletSize);
         }
+        private void InstantiateWeaponBullets(Bullet weaponBullet, uint weaponDamage, int bulletAmount)
+        {
+            for (int i = 0; i < bulletAmount; i++)
+            {
+                GameObject bullet = Instantiate(weaponBullet.gameObject);
+
+                bullet.GetComponent<Bullet>().Initialize(weaponDamage);
+                bullet.SetActive(false);
+                bullet.transform.SetParent(bulletSpawn.transform);
+
+                bullets.Add(bullet);
+            }
+        }
+        #endregion
+
+        #region PickUp and Drop
         public void PickUp()
         {
             if (!GetWeaponComponents())
@@ -50,14 +73,52 @@ namespace TechnicalTest.System.WeaponSystem.WeaponCore
                 weaponRigidbody.AddForce(weaponParent.forward * 7f);
             }
         }
+        #endregion
+
+        #region Get Weapon Info
         private bool GetWeaponComponents()
         {
             weaponRigidbody = GetComponent<Rigidbody>();
             weaponCollider = GetComponent<BoxCollider>();
-            weaponTrigger = triggerDetector.GetComponent<SphereCollider>();
+            weaponTrigger = transform.GetComponentInChildren<SphereCollider>();
 
             return weaponRigidbody && weaponCollider && weaponTrigger;
         }
-        public string WeaponName() => weaponType.ToString();
+        public string GetWeaponName() => weaponType.ToString();
+        #endregion
+
+        #region Weapon Bullets
+        private void Update()
+        {
+            attackSpeedTimer += Time.deltaTime;
+
+            if (attackSpeedTimer >= weaponAttackSpeed)
+                canShoot = true;
+        }
+        public void RequestBullet()
+        {
+            if (canShoot)
+            {
+                for (int i = 0; i < bullets.Count; i++)
+                {
+                    if (!bullets[i].activeSelf)
+                    {
+                        bullets[i].SetActive(true);
+                        bullets[i].transform.parent = null;
+                        bullets[i].GetComponent<Bullet>().SpawnBullet(bulletSpawn, this.transform);
+
+                        attackSpeedTimer = 0;
+                        canShoot = false;
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Attack speed limit");
+            }
+
+        }
+        #endregion
     }
 }
